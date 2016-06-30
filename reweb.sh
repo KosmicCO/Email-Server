3c065a11dad61a7d413da3fa406735ce8f8a8323
Cruz Barnum <cruzbarnum@gmail.com>
2016-06-29 18:20:06 -0700

    Update reweb.sh

--- reweb.sh
+++ reweb.sh
@@ -2,7 +2,7 @@
 
 sudo rm -r /var/www/html/
 sudo mkdir /var/www/html/
-sudo cp -r /tmp/Email-Server/* /var/www/html/
-sudo cp -r /tmp/Email-Server/.[^.]* /var/www/html/
+sudo cp -r /etc/web/Email-Server/* /var/www/html/
+sudo cp -r /etc/web/Email-Server/.[^.]* /var/www/html/
 sleep 1
 sudo service apache2 restart 
