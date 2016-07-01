a223349a5a3b1a292cf8010d145bef341c5df85c
root <root@ICCD.iccdproject.tech>
2016-06-30 17:19:50 -0700

    Starting to use Materialize.

---
+++ signup.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv = "Content-Type content = "text/html; charset = UTF-8" />
+        <meta name = "viewpoint" content = "width = device-width, initial-scale = 1, maximum-scale = 1.0, user-scalable = no">
+
+        <title>ICCD Sign-Up</title>
+
+        <link rel = "apple-touch-icon-precomposed" href = "image/favicon/apple-touch-icon-152x152.png">
+        <meta name = "msapplication-TitleColor" content = "#ffffff">
+        <meta name = "msapplication-TitleImage" content = "images/favicon/mstile-144x144.png">
+        <link rel = "icon" href = "images/favicon/favicon-32x32.png" sizes = "32x32">
+    </head>
+</html>
